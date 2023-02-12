b03bec45ae4343acc597496537f4ebf01014aad6
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2023-02-12 03:52:16 -0500

    mobility viz

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for,request,redirect, make_response
 import json
 import tailer
+import csv
 import pandas as pd
 from flask import Flask, render_template, make_response
 app = Flask(__name__)
@@ -17,8 +18,16 @@ def main():
     return render_template('index.html')
 
 @app.route("/search-trends")
-def economy():
+def search():
     return render_template("search_trends.html")
+
+@app.route("/trends")
+def trends():
+    return render_template("case-based-trends.html")
+
+@app.route("/mobility")
+def mobility():
+    return render_template("mobility.html")
 
 @app.context_processor
 def utility_processor():    
@@ -54,11 +63,31 @@ def utility_processor():
         strains = list(df['strain'])
 
         return [strain_dict,bar_labels,strains]
+    
+    def get_mobility_data():
+        mobility_monthly_data = []
+        with open("static/graph_data/mobility_monthly.csv", mode="r") as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                mobility_monthly_data.append(line)
+        # print(mobility_monthly_data)
+        return mobility_monthly_data
+    
+    def get_mobility_pie_data():
+        mobility_pie_data = []
+        with open("static/graph_data/mean_mobility.csv", mode="r") as file:
+                csvFile = csv.reader(file)
+                for line in csvFile:
+                    mobility_pie_data.append(line)
+        # print(mobility_pie_data)
+        return mobility_pie_data
         
     return dict(
     get_search_trends           = get_search_trends,
     get_top_search_trends = get_top_search_trends,
-    get_epidemiology_strains = get_epidemiology_strains)
+    get_epidemiology_strains = get_epidemiology_strains,
+    get_mobility_data = get_mobility_data,
+    get_mobility_pie_data = get_mobility_pie_data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
